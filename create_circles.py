b4f4db1d741ceb1db1e0b8d0e252846e3ccf5755
Given code <>
2022-03-18 13:23:53 -0500

    Adding social circles to DB

--- create_circles.py
+++ create_circles.py
@@ -1,6 +1,7 @@
 from neo4j import GraphDatabase
 import logging
 from neo4j.exceptions import ServiceUnavailable
+import os
 
 class App:
 
@@ -19,34 +20,34 @@ class App:
     @staticmethod
     def _create_and_return_circle(tx, circle):
         query = (
-            "MERGE (p1:Circle { name: $circle }) "
+            "MERGE (p1:Circle { id: $circle }) "
             "RETURN p1"
         )
         result = tx.run(query, circle=circle)
         try:
-            return [{"p1": row["p1"]["name"]} for row in result]
+            return [{"p1": row["p1"]["id"]} for row in result]
         except ServiceUnavailable as exception:
             logging.error("{query} raised an error: \n {exception}".format(
                 query=query, exception=exception))
             raise
 
-    def add_node_to_circle(self, circle, person_name):
+    def add_node_to_circle(self, circle, person_id):
         with self.driver.session() as session:
-            result = session.write_transaction(self._add_node_to_circle, circle, person_name)
+            result = session.write_transaction(self._add_node_to_circle, circle, person_id)
             for row in result:
                 print("Added node to circle : {p1}, {p2}".format(p1=row['p1'], p2=row['p2']))
 
     @staticmethod
-    def _add_node_to_circle(tx, circle, person_name):
+    def _add_node_to_circle(tx, circle, person_id):
         query = (
-            "MATCH (p1:Circle { name: $circle })"
-            "MERGE (p2:Personq { name: $person_name })"            
+            "MATCH (p1:Circle { id: $circle })"
+            "MERGE (p2:Person { id: $person_id })"            
             "MERGE (p2)-[r:IS_PART_OF_CIRCLE]->(p1)"
             "RETURN p1, p2"
         )
-        result = tx.run(query, circle=circle, person_name=person_name)
+        result = tx.run(query, circle=circle, person_id=person_id)
         try:
-            return [{"p1": row["p1"]["name"], "p2": row["p2"]["name"]} for row in result]
+            return [{"p1": row["p1"]["id"], "p2": row["p2"]["id"]} for row in result]
         except ServiceUnavailable as exception:
             logging.error("{query} raised an error: \n {exception}".format(
                 query=query, exception=exception))
@@ -56,16 +57,19 @@ if __name__ == "__main__":
     # Aura queries use an encrypted connection using the "neo4j+s" URI scheme
     uri = "neo4j+s://3cc782b6.databases.neo4j.io"
     user = "neo4j"
-    password = ""
+    password = "9-2lE7ssrtt09vCiRWoV8IH2F_RyiFlV7zSIEF5eiR0"
     app = App(uri, user, password)
-    file1 = open('0.circles', 'r')
-    for line in file1.readlines():
-        line = line.split()
-        circle_name = line[0]
-        app.create_circle(circle_name)
-        for person_name in line[1:]:
-            app.add_node_to_circle(circle_name, person_name)        
-
+    directory = "../facebook/"
+    for filename in os.listdir(directory):        
+        if filename.endswith(".circles"):            
+            file = open(directory+filename, 'r')
+            for line in file.readlines():
+                line = line.split()
+                circle_name = line[0]+"_"+filename[:len(filename)-8]
+                # print(circle_name)
+                app.create_circle(circle_name)
+                for person_id in line[1:]:
+                    app.add_node_to_circle(circle_name, person_id)                
     app.close()
 
 
